120cccb302eaa6b868af8a64882bc25783a18fb1
Fani <fanit6@gmail.com>
2024-05-02 20:18:06 +0200

    Checkboxes compleated

--- src/components/Predavaci.jsx
+++ src/components/Predavaci.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+import Checkbox from './Checkbox.jsx'
+
 function Predavaci() {
+  const themes = ["React", "Express", "PHP", "WordPress"];
+  const organisation = ["Digitalna Dalmacija", "Locastic", "Lorem ipsum"];
+
+  const [checkedThemes, setCheckedThemes] = useState(
+    new Array(themes.length).fill(false)
+  );
+
+  const [checkedOrganisation, setCheckedOrganisation] = useState(
+    new Array(organisation.length).fill(false)
+  );
+
+  const handleThemeOnChange = (position) => {
+    const updatedCheckedState = checkedThemes.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedThemes(updatedCheckedState);
+  };
+
+  const handleOrganisationOnChange = (position) => {
+    const updatedCheckedState = checkedOrganisation.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedOrganisation(updatedCheckedState);
+  };
 
   return (
     <>
-      <p>Predavaci</p>
+      <Checkbox
+        title="Teme"
+        category={themes}
+        checkedItems={checkedThemes}
+        handleOnChange={handleThemeOnChange}
+      />
+      <div style={{ marginTop: "20px" }}>
+      <Checkbox
+        title="Organizacija"
+        category={organisation}
+        checkedItems={checkedOrganisation}
+        handleOnChange={handleOrganisationOnChange}
+      />
+      </div>
     </>
   )
 }
--- src/components/Radionice.jsx
+++ src/components/Radionice.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+import Checkbox from './Checkbox.jsx'
+
 function Radionice() {
+  const themes = ["React", "Express", "PHP", "WordPress"];
+  const levels = ["Junior", "Mid", "Senior"];
+
+  const [checkedThemes, setCheckedThemes] = useState(
+    new Array(themes.length).fill(false)
+  );
+
+  const [checkedLevels, setCheckedLevels] = useState(
+    new Array(levels.length).fill(false)
+  );
+
+  const handleThemeOnChange = (position) => {
+    const updatedCheckedState = checkedThemes.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedThemes(updatedCheckedState);
+  };
+
+  const handleLevelOnChange = (position) => {
+    const updatedCheckedState = checkedLevels.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedLevels(updatedCheckedState);
+  };
 
   return (
     <>
-      <p>Radionice</p>
+      <Checkbox
+        title="Teme"
+        category={themes}
+        checkedItems={checkedThemes}
+        handleOnChange={handleThemeOnChange}
+      />
+      <div style={{ marginTop: "20px" }}>
+      <Checkbox
+        title="Težina"
+        category={levels}
+        checkedItems={checkedLevels}
+        handleOnChange={handleLevelOnChange}
+      />
+      </div>
     </>
   )
 }

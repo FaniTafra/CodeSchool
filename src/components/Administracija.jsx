a254475ea0f630a08779fdbced34b3f2fe819026
Fani <fanit6@gmail.com>
2024-05-05 21:37:26 +0200

    Admin rerouting

--- src/components/Administracija.jsx
+++ src/components/Administracija.jsx
@@ -1,4 +1,18 @@
+import { useAdminContext } from './AdminContext';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function Administracija() {
+  const {isAdmin} = useAdminContext()
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (!isAdmin) {
+      let path = `/`; 
+      navigate(path);
+    }
+  }, [isAdmin, navigate]);
 
   return (
     <>
